36c35b152b268bb237e7f2c62d0e1e3c50b1d059
Sudhakar-Maheshwaran <Sudhakar33mkr@gmail.com>
2024-02-15 20:04:21 +0530

    registration-form

---
+++ index.js
@@ -0,0 +1,48 @@
+const express=require("express")
+const bodyParser=require("body-parser")
+const mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/database')
+var db=mongoose.connection
+db.on('error',()=> console.log("error in connecting to database"))
+db.once('open',()=> console.log("connected to database"))
+
+app.post("/login.html",(req,res) =>{
+    var name=req.body.name
+    var email=req.body.email
+    var number=req.body.number
+    var password=req.body.password
+
+    var data={
+        "name":name,
+        "email":email,
+        "number":number,
+        "password":password
+    }
+    db.collection('database').insertOne(data,(err,collection) =>{
+        if(err){
+            throw err;
+        }
+        console.log("record inserted successful")
+    })
+    return res.redirect('/login.html')
+})
+
+app.get("/",(req,res) =>{
+    res.set({
+        "allow-acces-allow-orgin":'*'
+    })
+    return res.redirect("/signup.html")
+}).listen(3500);
+
+
+    console.log("listening on port 3500")  
+
